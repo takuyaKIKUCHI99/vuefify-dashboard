e274bef14d8be4a25cfa29b6086eaae60622e4cf
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-09-22 14:49:51 +0900

    Form validation

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -3,8 +3,14 @@
     <v-row>
       <v-col>
         <h1>Signup Page</h1>
-        <v-form>
-          <v-text-field label="Email" type="email" />
+        <v-form ref="signUpForm" v-model="formValidity">
+          <v-text-field
+            label="Email"
+            type="email"
+            v-model="email"
+            :rules="emailRules"
+          />
+
           <v-autocomplete label="Which browser do you use?" :items="browsers" />
           <v-file-input label="Attach profile picture" />
 
@@ -29,8 +35,25 @@
             <v-date-picker v-model="birthday" @input="birthdayMenu = false"></v-date-picker>
           </v-menu>
 
-          <v-checkbox label="Agree to terms & conditions" />
-          <v-btn type="submit" color="primary">Submit</v-btn>
+          <v-checkbox
+            label="Agree to terms & conditions"
+            v-model="agreeToTerms"
+            :rules="agreeToTermsRules"
+            required
+          />
+
+          <v-btn type="submit" color="primary" class="mr-4" :disabled="!formValidity">
+            Submit
+          </v-btn>
+          <v-btn color="success" class="mr-4" @click="validateForm">
+            Validate Form
+          </v-btn>
+          <v-btn color="warning" class="mr-4" @click="resetValidation">
+            Reset Validation
+          </v-btn>
+          <v-btn color="error" @click="resetForm">
+            Reset
+          </v-btn>
         </v-form>
       </v-col>
     </v-row>
@@ -42,11 +65,45 @@ export default {
   name: "Signup",
   data() {
     return {
-      date: new Date().toISOString().substr(0, 10),
+      // Form input
+      email: "",
       birthday: "",
+      agreeToTerms: false,
+      // Form component attributes
+      date: new Date().toISOString().substr(0, 10),
       birthdayMenu: false,
       browsers: ["Chrome", "Firefox", "Safari", "Edge", "Brave"],
+      formValidity: false,
+      // Rules
+      emailRules: [
+        (value) => value.indexOf("@") !== 0 || "Email should have a username.",
+        (value) => value.includes("@") || "Email should include an @ symbol.",
+        (value) =>
+          value.indexOf(".") - value.indexOf("@") > 1 ||
+          "Email should contain a valid domain.",
+        (value) =>
+          value.includes(".") || "Email should include a period symbol.",
+        (value) =>
+          value.indexOf(".") <= value.length - 3 ||
+          "Email should contain a valid domain extension.",
+      ],
+      agreeToTermsRules: [
+        (value) =>
+          !!value ||
+          "You must agree to the terms and conditions to sign up for an account.",
+      ],
     };
+  },
+  methods: {
+    resetForm() {
+      this.$refs.signUpForm.reset();
+    },
+    resetValidation() {
+      this.$refs.signUpForm.resetValidation();
+    },
+    validateForm() {
+      this.$refs.signUpForm.validate();
+    },
   },
 };
 </script>
