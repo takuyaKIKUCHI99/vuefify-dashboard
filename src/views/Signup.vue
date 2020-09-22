f5b079faaba30bceea3bd1e1949e93562eaadc6a
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-09-22 09:50:42 +0900

    Form Birthday enhancement

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -7,8 +7,28 @@
           <v-text-field label="Email" type="email" />
           <v-autocomplete label="Which browser do you use?" :items="browsers" />
           <v-file-input label="Attach profile picture" />
-          <v-text-field label="Birthday" v-model="birthday" readonly />
-          <v-date-picker v-model="birthday" />
+
+          <!-- Birthday -->
+          <v-menu
+            v-model="birthdayMenu"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            transition="scale-transition"
+            offset-y
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="birthday"
+                label="Birthday"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker v-model="birthday" @input="birthdayMenu = false"></v-date-picker>
+          </v-menu>
+
           <v-checkbox label="Agree to terms & conditions" />
           <v-btn type="submit" color="primary">Submit</v-btn>
         </v-form>
@@ -19,9 +39,12 @@
 
 <script>
 export default {
+  name: "Signup",
   data() {
     return {
+      date: new Date().toISOString().substr(0, 10),
       birthday: "",
+      birthdayMenu: false,
       browsers: ["Chrome", "Firefox", "Safari", "Edge", "Brave"],
     };
   },
